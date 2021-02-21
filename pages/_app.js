5431f43409df54389558a1880a42f7008e57051a
simon-ugor <simiugor@gmail.com>
2021-02-21 01:46:03 +0100

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
-// importing css
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import "../styles.css"
 
-// _app.js is rendering everything
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
-const App = ({Component, pageProps}) => {
-    return (
-        <Component {...pageProps} />
-    )
+  return <Component {...pageProps} />
 }
 
 export default App
